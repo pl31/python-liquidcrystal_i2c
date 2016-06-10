775cc757ce0b318818d23f9cdccada757faa4f1f
Patrick Büch <git@paco.pl31.de>
2016-06-10 16:30:40 +0200

    Remove german umlauts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='liquidcrystal_i2c',
       version='0.1',
       description='Library for the LCD-Display "LCM1602 IIC V2"',
-      author='Patrick Büch',
+      author='Patrick Buech',
       url='https://github.com/pl31/python-liquidcrystal_i2c',
       packages=['liquidcrystal_i2c'],
       license='MIT License',
